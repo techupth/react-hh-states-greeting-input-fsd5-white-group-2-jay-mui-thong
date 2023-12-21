7d46cc8a6381f3155d9e6f97b2a08b2f47fdae7d
SamitaTT <m.samita.tt@gmail.com>
2023-12-21 12:24:38 +0700

    feat: completed stages-input-greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputGreetingMsg, setInputGreetingMsg] = useState("");
+  const [greetingMsg, setGreetingMsg] = useState("Greeting Message");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMsg}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(event) => setInputGreetingMsg(event.target.value)}
+        />
       </div>
 
-      <div className="buttons">
+      <div
+        className="buttons"
+        onClick={() => {
+          setGreetingMsg(inputGreetingMsg);
+        }}
+      >
         <button>Update text</button>
       </div>
     </div>
